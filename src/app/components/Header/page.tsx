aed6e7b13a72ab9301af1270e7899d9fa3e2aa8b
Hadhemi33 <hadhemi.benmansour@isimg.tn>
2024-09-20 05:59:51 +0100

    Header &Navbar

---
+++ src/app/components/Header/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Link from "next/link";
+import React, { useState } from "react";
+import { usePathname } from "next/navigation";
+import styles from "./index.module.css";
+
+const Header: React.FC = () => {
+  const [nav, setNav] = useState<boolean>(false);
+  const pathname = usePathname();
+
+  return (
+    <div className={styles.aa_header}>
+      <div className={styles.container}>
+        <div className={styles.row}>
+          <div className="col-md-12">
+            <div className={styles.aa_header_area}>
+              <div className={styles.row}>
+                <div className="col-md-6 col-sm-6 col-xs-6">
+                  <div className={styles.aa_header_left}>
+                    <div className={styles.aa_telephone_no}>
+                      <span className={styles.fa_phone}> 📞</span>
+                      +971 58 560 6752
+                    </div>
+                    <div className={(styles.aa_email, styles.hidden_xs)}>
+                      <span className={styles.fa_envelope_o}> 📧</span> info@
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6 col-sm-6 col-xs-6">
+                  <div className={styles.aa_header_right}>
+                    <a href="/signup" className={styles.aa_register}>
+                      Register
+                    </a>
+                    <a href="/sigin" className={styles.aa_login}>
+                      Login
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/app/components/Navbar/index.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
+import styles from "./index.module.css";
+
+
+
+const Navbar: React.FC = () => {
+  const [nav, setNav] = useState<boolean>(false);
+  const pathname = usePathname();
+  const [scrolled, setScrolled] = useState(false);
+
+  const links = [
+    { id: 1, link: "/", name: "HOME" },
+    { id: 2, link: "/properties", name: "PROPERTIES" },
+    { id: 3, link: "/gallery", name: "GALLERY" },
+    { id: 4, link: "/blog", name: "BLOG" },
+    { id: 5, link: "/contact", name: "CONTACT" },
+  ];
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <nav className={`${styles.navbar} ${scrolled ? styles.scrolled : ""}`}>
+      <div className={styles.navbarContainer}>
+        <a href="/" className={styles.navbarLogoText}>
+          Deville Signature
+          <span className={styles.navbarLogoTextSpan}> Properties</span>
+        </a>
+
+        <div className={styles.navbarLinksContainer}>
+          {links.map(({ id, link, name }) => (
+            <h3
+              key={id}
+              className={`${styles.navbarLink} ${
+                pathname === link ? styles.activeLink : ""
+              }`}
+            >
+              <Link href={link}>{name}</Link>
+            </h3>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
